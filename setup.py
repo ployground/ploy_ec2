4343fbb82e2c219118f4dbbdc66550ce13f7b010
Florian Schulze <florian.schulze@gmx.net>
2014-03-20 10:55:46 +0100

    Moved ec2 into it's own plugin package.

--- setup.py
+++ setup.py
@@ -1,44 +1,26 @@
 from setuptools import setup
-import os
 
-version = "0.14"
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-HISTORY = open(os.path.join(here, 'HISTORY.rst')).read()
-
-install_requires = [
-    'setuptools',
-    'boto >= 2.0',
-    'Fabric >= 1.3.0',
-    'lazy']
-
-try:
-    import argparse
-    argparse    # make pyflakes happy...
-except ImportError:
-    install_requires.append('argparse >= 1.1')
+version = "0.1"
 
 setup(
     version=version,
-    description="A script allowing to setup Amazon EC2 instances through configuration files.",
-    long_description=README + "\n\n" + HISTORY,
-    name="mr.awsome",
+    description="A plugin for mr.awsome providing integration with Amazon EC2.",
+    name="mr.awsome.ec2",
     author='Florian Schulze',
     author_email='florian.schulze@gmx.net',
-    url='http://github.com/fschulze/mr.awsome',
+    url='http://github.com/fschulze/mr.awsome.ec2',
     include_package_data=True,
     zip_safe=False,
     packages=['mr'],
     namespace_packages=['mr'],
-    install_requires=install_requires,
+    install_requires=[
+        'setuptools',
+        'mr.awsome',
+        'boto >= 2.0'
+    ],
     setup_requires=[
         'setuptools-git'],
     entry_points="""
-        [console_scripts]
-        aws = mr.awsome:aws
-        assh = mr.awsome:aws_ssh
         [mr.awsome.plugins]
-        plain = mr.awsome.plain:plugin
         ec2 = mr.awsome.ec2:plugin
     """)
